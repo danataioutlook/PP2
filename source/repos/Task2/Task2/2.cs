b41c3f300d703fe33be9270a7b83921c42d081bb
Dana <danaturgan000@gmail.com>
2019-02-16 23:14:36 +0600

    Init

---
+++ source/repos/Task2/Task2/2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            name = Console.ReadLine();
+            string id;
+            id = Console.ReadLine();
+            int num;
+            num = int.Parse(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("Name" + ":" + name);
+            Console.WriteLine("ID" + ":" + id);
+            if (num == 1)
+            {
+                num++;
+            }
+
+            Console.WriteLine("Year" + ":" + num);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ source/repos/Task3/Task3/3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+            a[0] = 1;
+            a[1] = 2;
+            a[2] = 3;
+
+
+           for(int i = 0; i < a.Length; i++)
+            {
+                for(int j = 2; j <= i; j++)
+                {
+                    Console.WriteLine(a[i] + " ");
+                }
+            }
+        }
+    }
+}
